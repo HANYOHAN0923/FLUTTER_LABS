35089776c7a5e7aa78a5d6a1b92b99c64176eaf6
john Han <gksdtgks2124@gmail.com>
2021-12-17 11:13:05 +0900

    split & if statement for widget

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/quiz.dart';
 
-import './question.dart';
-import './answer.dart';
+import 'quiz.dart';
+import 'result.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,9 +16,30 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _questionList = const [
+    {
+      'questionText': 'Choose an odd number',
+      'answers': ['4', '100', '7', '36']
+    },
+    {
+      'questionText': 'Choose an even number',
+      'answers': ['99', '25', '1', '404']
+    },
+    {
+      'questionText': 'Choose an Natural Number',
+      'answers': ['3.14', '-24.2', '0', '-100']
+    },
+  ];
+
   int _questionIndex = 0;
 
   void _textChanger() {
+    if (_questionIndex < _questionList.length) {
+      print('We have more questions!');
+    } else {
+      print('No More Questions!');
+    }
+
     setState(() {
       _questionIndex++;
     });
@@ -26,37 +48,22 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    final questionList = [
-      {
-        'questionText': 'Choose an odd number',
-        'answers': ['4', '100', '7', '36']
-      },
-      {
-        'questionText': 'Choose an even number',
-        'answers': ['99', '25', '1', '404']
-      },
-      {
-        'questionText': 'Choose an Natural Number',
-        'answers': ['3.14', '-24.2', '0', '-100']
-      },
-    ];
-
     return MaterialApp(
+      // if condotion ?
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questionList[_questionIndex]['questionText'] as String,
-            ),
-            ...(questionList[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_textChanger, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < _questionList.length
+            // 가독성을 위해서, 더 위젯은 더 적고, 디테일 할 수록 좋음
+            ? Quiz(
+                textChanger: _textChanger,
+                questionIndex: _questionIndex,
+                questionList: _questionList,
+              )
+            // else block : => include another Widgets
+            // Center => has child
+            : Result(),
       ),
     );
   }
